3cc563bb315103cf88ba307912959eea9b230f44
j-iyeon <jynvnvgl33@naver.com>
2025-11-22 14:39:30 +0900

    COLOR class 정의

---
+++ src/COLOR.h
@@ -0,0 +1,26 @@
+#pragma once
+// enum값을 enum 클래스로 변환
+
+/**
+* COLOR 클래스
+* 색 초기화하는 클래스
+*/
+enum class COLOR {
+	BLACK,      /*  0 : 까망 */
+	DARK_BLUE,    /*  1 : 어두운 파랑 */
+	DARK_GREEN,    /*  2 : 어두운 초록 */
+	DARK_SKY_BLUE,  /*  3 : 어두운 하늘 */
+	DARK_RED,    /*  4 : 어두운 빨강 */
+	DARK_VOILET,  /*  5 : 어두운 보라 */
+	DARK_YELLOW,  /*  6 : 어두운 노랑 */
+	GRAY,      /*  7 : 회색 */
+	DARK_GRAY,    /*  8 : 어두운 회색 */
+	BLUE,      /*  9 : 파랑 */
+	GREEN,      /* 10 : 초록 */
+	SKY_BLUE,    /* 11 : 하늘 */
+	RED,      /* 12 : 빨강 */
+	VOILET,      /* 13 : 보라 */
+	YELLOW,      /* 14 : 노랑 */
+	WHITE,      /* 15 : 하양 */
+};
+
